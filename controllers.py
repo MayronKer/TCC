de48d9bc7be4bbd884c17a47e2cbdf8bbeb62833
Mayron <mayronkersbaumgmail.com>
2021-01-30 14:46:24 -0300

    -> Adaptando cadastro e logia para fazer crud

--- controllers.py
+++ controllers.py
@@ -5,3 +5,7 @@ from flask import redirect, url_for
 @app.route('/acao_login', methods=["POST", "GET"])
 def acao_login():
     return redirect('sobre.html')
+
+@app.route('/acao_cadastro', methods=["POST", "GET"])
+def acao_cadastro():
+    return redirect('sobre.html')
